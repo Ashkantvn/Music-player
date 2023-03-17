efdc1ed6d92555738bcbfff7718f80ed5ce351dc
AshkanTavangar <ashkantavangar7@gmail.com>
2023-03-18 01:23:25 +0330

    Show (is playing)message if the song is paying

--- src/parts/musicControl/MusicControl.jsx
+++ src/parts/musicControl/MusicControl.jsx
@@ -3,14 +3,23 @@ import React, { useState } from 'react'
 const MusicControl = ({songinformation})=>{
     const [isClicked, setisClicked] = useState(false)
     const {title,type,url} = songinformation;
+    const [isplay , setisplay] = useState(false);
+
+    const playHandler = ()=>{
+        setisplay(isplay=>isplay=true);
+    }
+    const pauseHandler = ()=>{
+        setisplay(isplay=>isplay=false);
+    }
     return(
         <div className='text-center flex flex-col gap-5 shadow-lg p-9 '>
             <button onClick={()=>{setisClicked(!isClicked)}}>{title}</button>
             
-            <audio controls={isClicked}>
+            <audio controls={isClicked} onPlay={playHandler} onPause={pauseHandler}>
                 <source src={url} type={type}/>
                 Your browser does not support audio tag
             </audio>
+            {isplay && <h1 className='text-orange-300'>Is playing...</h1>}
         </div>
     )
 }
