62d16169c9a5fe743fc37c6c0dbf946972df8cb6
AshkanTavangar <ashkantavangar7@gmail.com>
2023-03-17 13:40:00 +0330

    Change style of fileInput and refactor it

--- src/parts/fileInput/FileInput.jsx
+++ src/parts/fileInput/FileInput.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {HiUpload} from "react-icons/hi"
+import { HiUpload } from "react-icons/hi";
 
 const FileInput = ({ fileStorage }) => {
-  const { uploadedfile, setuploadedfile,setarrayFiles } = fileStorage;
+  const { uploadedfile, setuploadedfile, setarrayFiles } = fileStorage;
 
   // Event handlers
   const inputHandler = (e) => {
@@ -10,17 +10,28 @@ const FileInput = ({ fileStorage }) => {
   };
 
   const addButtonHandler = () => {
-    if(uploadedfile){
-      setarrayFiles(oldArrayFiles=>[...oldArrayFiles,uploadedfile]);
+    if (uploadedfile) {
+      setarrayFiles((oldArrayFiles) => [...oldArrayFiles, uploadedfile]);
       setuploadedfile("");
     }
   };
 
   return (
     <>
-      <label htmlFor="files" className="flex items-center"><HiUpload/><span className=" ml-1">Choose a file</span></label>
-      <input id="files" className=" hidden" type="file" onChange={inputHandler} />
-      <button className="my-6" onClick={addButtonHandler}>Add</button>
+      <label htmlFor="files" className="cursor-pointer flex items-center">
+        <HiUpload />
+        <span className=" ml-1">Choose a file</span>{" "}
+        <input
+          id="files"
+          className=" hidden"
+          type="file"
+          onChange={inputHandler}
+        />
+      </label>
+
+      <button className="my-6" onClick={addButtonHandler}>
+        Add
+      </button>
     </>
   );
 };
